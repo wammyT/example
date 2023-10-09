1b4e176bd37a85b460d82d0d875bb9b0aa4ebabb
tamara walling <tamara_walling@yahoo.com>
2023-10-09 16:06:46 -0700

    variables

--- holbertonschool-low_level_programming/variables_if_else_while/1-last_digit.c
+++ holbertonschool-low_level_programming/variables_if_else_while/1-last_digit.c
@@ -7,7 +7,7 @@
  *
  * Return: Always (Success)
  */
-int main (void)
+int main(void)
 {
 	int n, lastd;
 
@@ -19,7 +19,7 @@ int main (void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
-	else if (lasttd ==0)
+	else if (lastd == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
